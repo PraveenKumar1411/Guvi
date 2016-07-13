a518488849fc0c7c4b19c3e4bb260b1610748056
PraveenKumar1411 <praveenero38@gmail.com>
2016-07-13 18:51:01 +0530

    Create Even_Numbers_Between_Numbers.java

---
+++ Even_Numbers_Between_Numbers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Even_Numbers_Between_Numbers {
+
+	public static void main(String[] args) {
+		System.out.println("Enter the values:");
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();
+		if(a>0 &&b>0){
+		for(int i=a;i<b;i++){
+			if(i%2==0){
+				System.out.println(i);
+				
+			}
+			else{
+				continue;
+			}
+		}
+		}
+		else{
+			System.out.println("Enter the valid Numbers:");
+			
+		}
+		
+	}
+
+}
