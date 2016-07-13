59abf809dee19293c29e9ca6b0cd4c8921e8c5d2
PraveenKumar1411 <praveenero38@gmail.com>
2016-07-13 18:34:10 +0530

    Create Counting_No_Of_Digits.java

---
+++ Counting_No_Of_Digits.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Counting_No_Of_Digits {
+
+	public static void main(String[] args) {
+		System.out.println("Enter an Intger");
+		Scanner in=new Scanner(System.in);
+		String  n=in.nextLine();
+		System.out.println("The no of digits in the given number="+n.length());
+
+
+	}
+
+}
