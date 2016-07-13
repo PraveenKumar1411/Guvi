8a14c00e8298f4247d0d7479d5cf3c91e6e515d9
PraveenKumar1411 <praveenero38@gmail.com>
2016-07-13 17:16:41 +0530

    Create Vowel_Or_Consonant.java

---
+++ Vowel_Or_Consonant.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Vowel_Or_Consonant {
+   public static void main(String args[]){
+	   System.out.println("Enter a character:");
+	   Scanner in=new Scanner(System.in);
+	   int count=0;
+	   char c = in.next().charAt(0);
+	   char vowel[]={'a', 'e','i', 'o', 'u'}; 
+	   for(int i=0;i<=4;i++){
+		   if(c==vowel[i]){
+			  
+			    count=1;
+			   break;
+		   }
+		   else{
+			  
+			    count=0;
+			   continue;
+			   
+		   }
+	   }
+	   if(count==1){
+		   System.out.println("The given character is a vowel");
+	   }
+   
+   else{
+	   System.out.println("The given character is not a vowel");
+   }
+}
+}
