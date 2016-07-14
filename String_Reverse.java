94b3de2db58da0f089f6b78149fdbf4b21a35801
PraveenKumar1411 <praveenero38@gmail.com>
2016-07-14 12:02:07 +0530

    Create String_Reverse.java

---
+++ String_Reverse.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class String_Reverse {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a String:");
+		Scanner in=new Scanner(System.in);
+	    String str=in.nextLine();
+	    char c[]=str.toCharArray();
+	    System.out.println("The Reversed String:");
+	    for(int i=c.length-1;i>=0;i--){
+	    	System.out.print(c[i]);
+	    }
+			}
+		}
+		
+
+	
+
+
